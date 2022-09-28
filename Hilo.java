00ece73818cf1f93c1502057214512dba83e45fa
felipecorella <112510495+felipecorella@users.noreply.github.com>
2022-09-28 11:12:07 -0700

    Add files via upload

---
+++ Hilo.java
@@ -0,0 +1,19 @@
+
+package factorial;
+
+public class Hilo implements Runnable {
+
+    private int numeros;
+
+    public Hilo(int n) {
+        this.numeros = n;}
+
+    public void run () {
+        int prod = 1;
+            for(int i=numeros; i > 0; i--){
+                prod = prod * i;
+            }
+        System.out.println(Thread.currentThread().getName() + ":" + prod);
+    }
+}  
+
